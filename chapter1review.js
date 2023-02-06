217702d9eb38fc3c91d4624832bbda7133c72188
6exf <6exf@fresnou.org>
2023-02-06 13:41:38 -0800

    Initial Commit

---
+++ chapter1review.js
@@ -0,0 +1,4 @@
+console.log("Hello" + "\nWorld!");
+console.log(17+2);
+console.log(17%2);
+console.log(`17+2 = ${17+2}`);
---
+++ dataTypes.js
@@ -0,0 +1,36 @@
+console.log(7);
+
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+console.log(0);
+console.log(7);
+
+//Floating Point Numbers (Floats)
+console.log(3.14);
+console.log(-0.123456);
+
+//Strings
+//Characters, letters, symbols, spaces
+console.log("Hi Earth");
+console.log('!$%# is also a string');
+console.log(`backticks by the 1 `);
+
+//Arithmetic Operators (math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //shift-8 gives *
+console.log(14/2);
+console.log(14%2); //% is called MOD
+                   //it means remainder after dividing
+
+//Concatenation means to combine settings
+console.log("My name is"+" Marston.");
+console.log('My'+' '+'age'+' '+' is' +14+ '.' );
+console.log("Your age is \n16.");
+
+//Template Literal - plug in some calculated value
+//Use backticks `` by the 1 on your keyboard
+console.log(`15 divided by 10 = ${15/10} asdfasdf`);
